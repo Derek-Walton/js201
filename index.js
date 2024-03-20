740c16889b35968c32644dbbf9b97f9725a2e517
Derek Walton <up2124866@myport.ac.uk>
2024-03-20 01:33:56 +0000

    completed 10/10

--- index.js
+++ index.js
@@ -13,15 +13,15 @@
 'use strict';
 
 function id() {
-  // e.g. return "UP654321";
+  return "UP2124866";
 }
 
 function fn() {
-  return 'Replace This With Your First Name';
+  return 'Derek';
 }
 
 function sn() {
-  return 'Replace This With Your Surname';
+  return 'Walton';
 }
 
 function example() {
@@ -29,4 +29,122 @@ function example() {
   // your first function then
   // add more below as necessary.
 
+}
+
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function checkObject(obj){
+  obj['checked'] = true;
+}
+
+function checkObjectInside(obj){
+  if ('data' in obj){
+    obj.data.checked = true;
+  }
+}
+
+function arraySet(arr, i, n){
+  // console.log(arr, i, n)
+  if (i <= arr.length - 1 && i >= 0 && Number.isInteger(i)){
+    arr[i] = n;
+  }
+}
+
+function addAll(arr){
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+
+  }
+  return sum
+}
+
+function larger(a, b){
+  if (a > b){
+    return a;
+  } else {
+    return b;
+  }
+}
+
+function largest(arr){
+  let largestNumber;
+  if (arr.length == 0){
+    return null;
+  } else{
+    for (let i = 0; i < arr.length; i++){
+      largestNumber = larger(largestNumber, arr[i]);
+    }
+
+  }
+  return largestNumber;
+}
+
+function compare(a, b){
+  if (a.length == b.length){
+    for (let i = 0; i < a.length; i++){
+      if (a[i] != b[i]){
+        return false
+      }
+    }  
+    return true;
+  }
+  return false
+  
+}
+
+function addToAll(arr, n){
+  for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i] + n;
+  }
+  return arr;
+}
+
+let remembered;
+
+function rememberThis(keepsake){
+  remembered = keepsake;
+}
+
+function nArray(n){
+  let newArr = [];
+  for (let i = 1; i < n + 1; i++){
+    newArr[i-1] = i;
+  }
+  return newArr;
+}
+
+function addAllOpt(arr){
+  let sum = 0;
+  if (arr == null){
+    return sum;
+  }
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function divisors(arr, div){
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % div == 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+function multiples(n, m){
+  let newArr = [];
+  for (let i = m; i <= n*m; i += m){
+    newArr.push(i);
+  }
+  return newArr;
 }
